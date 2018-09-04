828bc15991aa6c7aea2c65dbe3fa15b102b9e74e
Chris Davis <christopherjdavis417@gmail.com>
2018-09-04 19:54:07 -0400

    initial commit

---
+++ MyName1/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyName1
+{
+    class Person
+    {
+        public string Name;
+        public string Age;
+        public string Gender;
+
+        public Person()
+        {
+
+        }
+
+     
+
+
+    }
+}
--- MyName1/Program.cs
+++ MyName1/Program.cs
@@ -10,6 +10,23 @@ namespace MyName1
     {
         static void Main(string[] args)
         {
+
+            Person user = new Person();
+
+            Console.Write("What is your name? ");
+            user.Name = Console.ReadLine();
+
+            Console.Write("How old are you? ");
+            user.Age = Console.ReadLine();
+
+            Console.Write("What is your gender? ");
+            user.Gender = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + user.Name + ". " + user.Age + "???");
+            Console.WriteLine("Dang, you're old, " + user.Gender + ".");
+
+            Console.WriteLine("(press any key to end)");
+            Console.ReadLine();
         }
     }
 }
